68137541aabdc4c5c9f6c8ca20e262c2c9bc613a
Marcin Czenko <marcin@everydayproductive.com>
2019-06-19 15:11:21 +0200

    Uses auto for height for images

--- workspaces/homepage/src/components/ui-blocks/Img2.js
+++ workspaces/homepage/src/components/ui-blocks/Img2.js
@@ -3,11 +3,10 @@ import styled from '@emotion/styled'
 const Img2 = styled.img({
   margin: '0 0 0 0',
   width: '40%',
-  height: '40%',
+  height: 'auto',
   minWidth: '250px',
   '@media (max-width: 400px)': {
     width: '70%',
-    height: '70%',
     minWidth: '220px'
   }
 })
