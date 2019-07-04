6a4f88e31bd363a12460351417c559ddf15b67af
Marcin Czenko <marcin@everydayproductive.com>
2019-07-04 15:27:48 +0200

    Adds expo hack to support yarn workspaces

--- workspaces/telepath/source/index.test.js
+++
@@ -1,6 +0,0 @@
-import * as index from './index'
-import * as main from './main'
-
-it('exports the same things as index.js does', () => {
-  expect(Object.keys(main)).toEqual(Object.keys(index))
-})
