1604476baa685457895df3945e253448ee5300f4
Marcin Czenko <marcin@everydayproductive.com>
2019-07-25 19:38:54 +0200

    Persisting identity descriptor in the SecureStore

--- workspaces/idapp/src/views/identity/Identity.js
+++ workspaces/idapp/src/views/identity/Identity.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useCallback } from 'react'
 import base64url from 'base64url'
 import nacl from 'tweetnacl'
 import { Button } from 'react-native'
+import * as SecureStore from 'expo-secure-store'
 
 import { useTelepath } from './useTelepath'
 import { createIdentity } from './createIdentity'
@@ -23,9 +24,51 @@ const Identity = () => {
 
   channel.current = useTelepath(message => {
     console.log('received message: ', message)
+    if (message.method === 'set_identity' && message.params && message.params.length === 1) {
+      const { identity } = message.params[0]
+      persistIdentity(identity)
+    }
   }, error => {
     console.log('error: ', error)
   })
+
+  const persistIdentity = async ({ did, name }) => {
+    console.log('encryptionKeyPair.publicKey:', Buffer.from(encryptionKeyPair.current.publicKey).toString('hex'))
+    console.log('encryptionKeyPair.secretKey:', Buffer.from(encryptionKeyPair.current.secretKey).toString('hex'))
+    console.log('signingKeyPair.publicKey:', Buffer.from(signingKeyPair.current.publicKey).toString('hex'))
+    console.log('signingKeyPair.secretKey:', Buffer.from(signingKeyPair.current.secretKey).toString('hex'))
+    const encryptionKey = {
+      publicKeyBase64: base64url.encode(encryptionKeyPair.current.publicKey),
+      secretKeyBase64: base64url.encode(encryptionKeyPair.current.secretKey)
+    }
+    const signingKey = {
+      publicKeyBase64: base64url.encode(signingKeyPair.current.publicKey),
+      secretKeyBase64: base64url.encode(signingKeyPair.current.secretKey)
+    }
+    const key = base64url.encode(name)
+    const value = base64url.encode(JSON.stringify({
+      did,
+      name,
+      encryptionKey,
+      signingKey
+    }))
+    console.log('key:', key)
+    console.log('value:', value)
+    try {
+      await SecureStore.setItemAsync(key, value)
+      const v = await SecureStore.getItemAsync(key)
+      const identity = JSON.parse(base64url.decode(v))
+      const { did, name, encryptionKey, signingKey } = identity
+      console.log('did:', did)
+      console.log('name:', name)
+      console.log('encryptionKey.publicKey:', base64url.toBuffer(encryptionKey.publicKeyBase64).toString('hex'))
+      console.log('encryptionKey.secretKey:', base64url.toBuffer(encryptionKey.secretKeyBase64).toString('hex'))
+      console.log('signingKey.publicKey:', base64url.toBuffer(signingKey.publicKeyBase64).toString('hex'))
+      console.log('signingKey.secretKey:', base64url.toBuffer(signingKey.secretKeyBase64).toString('hex'))
+    } catch (e) {
+      console.error(e)
+    }
+  }
 
   const createSigningKeyPair = async () => {
     const secret = await randomBytes(nacl.sign.publicKeyLength)
