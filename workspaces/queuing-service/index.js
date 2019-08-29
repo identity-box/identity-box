e478d9224dd86b23d4c4104f6761a23ea508ecb0
Marcin Czenko <marcin@everydayproductive.com>
2019-08-29 19:13:16 +0200

    Updated zeit now setup for proper deployment of hush-hush

--- workspaces/queuing-service/index.js
+++ workspaces/queuing-service/index.js
@@ -1,11 +1,10 @@
 import createServer from './source/server'
-import { buildArgv } from 'jest-cli/build/cli'
 
 const server = createServer()
 
 let port = 3000
 
-if (buildArgv.length === 4 && (process.argv[2] === '--port' || process.argv[2] === '--p')) {
+if (process.argv.length === 4 && (process.argv[2] === '--port' || process.argv[2] === '--p')) {
   const p = Number(process.argv[3])
   port = isNaN(p) ? 3000 : p
 }
