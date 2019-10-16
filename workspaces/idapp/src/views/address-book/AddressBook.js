146061e29195dd700d24750dac0b90843c2a8c9f
Marcin Czenko <marcin@everydayproductive.com>
2019-10-16 17:31:50 +0200

    Address Book redesigned to come up with a similar style as when switching identities. Also reserves fixed space for Own and Peer Identities.

--- workspaces/idapp/src/views/address-book/AddressBook.js
+++ workspaces/idapp/src/views/address-book/AddressBook.js
@@ -1,46 +1,29 @@
 import React, { useRef, useState, useCallback } from 'react'
 import { useTheme } from 'react-navigation'
-import { SectionList, TouchableOpacity } from 'react-native'
+import { Text, View, FlatList, TouchableOpacity } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import styled from '@emotion/native'
 
 import { useIdentity } from 'src/identity'
+import { ThemeConstants } from 'src/theme'
 
 import { IdentityCell, EmptyIdentityCell } from './IdentityCell'
 
 const Container = styled.View({
-  flex: 1
+  flex: 1,
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%'
 })
-
-const Separator = styled.View({
-  borderBottomWidth: 1,
-  borderBottomColor: '#aaa',
-  marginLeft: 10
-})
-
-const Header = ({ children, ...rest }) => {
-  const theme = useTheme()
-  return (
-    <HeaderText {...rest} theme={theme}>{children}</HeaderText>
-  )
-}
-
-const HeaderText = styled.Text(({ theme }) => ({
-  color: 'white',
-  backgroundColor: theme === 'light' ? 'black' : '#1a1a1a',
-  fontSize: 16,
-  paddingTop: 10,
-  paddingBottom: 10,
-  paddingLeft: 10,
-  paddingRight: 10
-}))
 
 const AddressBook = ({ navigation }) => {
   const identityManager = useRef(undefined)
   const [identityNames, setIdentityNames] = useState([])
   const [identities, setIdentities] = useState({})
   const [peerIdentities, setPeerIdentities] = useState({})
+  const theme = useTheme()
 
   useIdentity({
     onReady: idManager => {
@@ -58,39 +41,135 @@ const AddressBook = ({ navigation }) => {
     }
   })
 
-  const onSelect = useCallback((item, section) => {
-    let identity
-    if (section.title === 'Your identities') {
-      const id = identities[item]
-      identity = { name: id.name, did: id.did, isOwn: true }
-    } else {
-      identity = { name: item, did: peerIdentities[item] }
-    }
+  const onSelectPeerIdentity = useCallback((item) => {
+    const identity = { name: item, did: peerIdentities[item] }
     navigation.navigate('IdentityDetails', identity)
-  }, [identities, peerIdentities])
+  }, [peerIdentities])
+
+  const onSelectOwnIdentity = useCallback(item => {
+    const id = identities[item]
+    const identity = { name: id.name, did: id.did, isOwn: true }
+    navigation.navigate('IdentityDetails', identity)
+  }, [identities])
+
+  const getCellBackgroundColor = useCallback((index, theme) => {
+    if (index % 2) {
+      return theme === 'light' ? 'white' : '#222'
+    } else {
+      return theme === 'light' ? '#eee' : '#111'
+    }
+  }, [])
+
+  const renderItem = useCallback(({ item, index }, onSelect) => {
+    if (item === 'No identities yet!') {
+      return <EmptyIdentityCell>{item}</EmptyIdentityCell>
+    } else {
+      return (
+        <IdentityCell
+          onSelect={onSelect}
+          textAlign='center'
+          backgroundColor={getCellBackgroundColor(index, theme)}
+        >
+          {item}
+        </IdentityCell>
+      )
+    }
+  }, [theme])
 
   return (
-    <Container>
-      <SectionList
-        sections={[
-          { title: 'Your identities', data: identityNames.length > 0 ? identityNames : ['No identities yet!'] },
-          { title: 'Peer identities', data: Object.keys(peerIdentities).length > 0 ? Object.keys(peerIdentities) : ['No identities yet!'] }
-        ]}
-        renderItem={({ item, section }) => {
-          if (item === 'No identities yet!') {
-            return <EmptyIdentityCell>{item}</EmptyIdentityCell>
-          } else {
-            return <IdentityCell onSelect={item => onSelect(item, section)}>{item}</IdentityCell>
-          }
+    <Container style={{
+      backgroundColor: theme === 'light' ? 'white' : '#111'
+    }}
+    >
+      <View style={{
+        width: '100%',
+        height: '45%'
+      }}
+      >
+        <View style={{
+          marginTop: 10,
+          alignSelf: 'center',
+          paddingLeft: 5,
+          borderLeftColor: ThemeConstants[theme].accentColor,
+          borderLeftWidth: 3
         }}
-        renderSectionHeader={({ section }) => (
-          <Header>{section.title}</Header>
-        )}
-        keyExtractor={(item, index) => index}
-        ItemSeparatorComponent={() => (
-          <Separator />
-        )}
-      />
+        >
+          <Text style={{
+            color: theme === 'light' ? 'black' : 'white',
+            fontSize: 17,
+            fontWeight: 'bold'
+          }}
+          >
+          Your identities
+          </Text>
+        </View>
+        <View style={{
+          flexGrow: 1,
+          width: '90%',
+          borderWidth: theme === 'light' ? 2 : 1,
+          borderColor: theme === 'light' ? ThemeConstants[theme].accentColor : '#333',
+          borderRadius: 10,
+          margin: 10,
+          paddingTop: 10,
+          paddingBottom: 10,
+          marginBottom: 20
+        }}
+        >
+          <FlatList
+            style={{
+              backgroundColor: theme === 'light' ? 'white' : '#111'
+            }}
+            data={identityNames.length > 0 ? identityNames : ['No identities yet!']}
+            renderItem={args => renderItem(args, onSelectOwnIdentity)}
+            keyExtractor={(item, index) => item}
+          />
+
+        </View>
+      </View>
+      <View style={{
+        width: '100%',
+        height: '45%'
+      }}
+      >
+        <View style={{
+          marginTop: 10,
+          alignSelf: 'center',
+          paddingLeft: 5,
+          borderLeftColor: ThemeConstants[theme].accentColor,
+          borderLeftWidth: 3
+        }}
+        >
+          <Text style={{
+            color: theme === 'light' ? 'black' : 'white',
+            fontSize: 17,
+            fontWeight: 'bold'
+          }}
+          >
+          Peer identities
+          </Text>
+        </View>
+        <View style={{
+          flexGrow: 1,
+          width: '90%',
+          borderWidth: theme === 'light' ? 2 : 1,
+          borderColor: theme === 'light' ? ThemeConstants[theme].accentColor : '#333',
+          borderRadius: 10,
+          margin: 10,
+          paddingTop: 10,
+          paddingBottom: 10,
+          marginBottom: 20
+        }}
+        >
+          <FlatList
+            style={{
+              backgroundColor: theme === 'light' ? 'white' : '#111'
+            }}
+            data={Object.keys(peerIdentities).length > 0 ? Object.keys(peerIdentities) : ['No identities yet!']}
+            renderItem={args => renderItem(args, onSelectPeerIdentity)}
+            keyExtractor={(item, index) => item}
+          />
+        </View>
+      </View>
     </Container>
   )
 }
