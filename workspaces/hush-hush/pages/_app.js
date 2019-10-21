4d9050a334392be1975b31d6e63be53be7968146
Marcin Czenko <marcin@everydayproductive.com>
2019-10-21 17:39:41 +0200

    [Hush-Hush, Next] Removes depreciated <Container> component in pages/_app.js

--- workspaces/hush-hush/pages/_app.js
+++ workspaces/hush-hush/pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import { Global } from '@emotion/core'
-import App, { Container } from 'next/app'
+import App from 'next/app'
 import 'semantic-ui-css/semantic.min.css'
 
 export default class MyApp extends App {
@@ -9,7 +9,7 @@ export default class MyApp extends App {
     const { Component, pageProps } = this.props
 
     return (
-      <Container>
+      <>
         <Head>
           <title>Hush Hush</title>
           <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
@@ -24,7 +24,7 @@ export default class MyApp extends App {
         }}
         />
         <Component {...pageProps} />
-      </Container>
+      </>
     )
   }
 }
