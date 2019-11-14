d77a8e7736a30e6b4cf3460a134df731d7fc8cf2
Marcin Czenko <marcin@everydayproductive.com>
2019-11-14 23:28:54 +0100

    Updates homepage to use new gatsby-confluenza-theme

--- workspaces/homepage/gatsby-config.js
+++ workspaces/homepage/gatsby-config.js
@@ -4,68 +4,9 @@ module.exports = {
     editBaseUrl: 'https://github.com/identity-box/identity-box/blob/master'
   },
   plugins: [
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/../../`,
-        ignore: [
-          '**/.git/**',
-          '**/coverage/**',
-          'workspaces/assets/**',
-          '**/node_modules/**',
-          '**/.cache/**',
-          '**/public/**',
-          '**/es/**',
-          '**/lib/**',
-          '**/umd/**'
-        ]
-      }
-    },
-    'gatsby-plugin-sharp',
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          'gatsby-remark-autolink-headers',
-          {
-            resolve: 'gatsby-remark-prismjs',
-            options: {
-              // Class prefix for <pre> tags containing syntax highlighting;
-              // defaults to 'language-' (eg <pre class="language-js">).
-              // If your site loads Prism into the browser at runtime,
-              // (eg for use with libraries like react-live),
-              // you may use this to prevent Prism from re-processing syntax.
-              // This is an uncommon use-case though;
-              // If you're unsure, it's best to use the default value.
-              classPrefix: 'language-',
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
-              inlineCodeMarker: null
-            }
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: '600px'
-            }
-          }
-        ]
-      }
-    },
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography'
-      }
-    },
+    '@confluenza/gatsby-theme-confluenza',
     'gatsby-plugin-emotion',
     'gatsby-plugin-catch-links',
-    'gatsby-plugin-layout',
     'gatsby-plugin-root-import'
   ]
 }
--- workspaces/homepage/src/layouts/documentation/normalizeLocationPath.js
+++
@@ -1,9 +0,0 @@
-const normalizeLocationPath = location => {
-  const normalizedPath = location.pathname.replace(/\/$/, '')
-  return {
-    path: normalizedPath,
-    pathWithHash: `${normalizedPath}${location.hash}`
-  }
-}
-
-export { normalizeLocationPath }
