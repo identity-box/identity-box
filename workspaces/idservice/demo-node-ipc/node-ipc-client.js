9dd5d3eb10b66d86fba5a0e9ea8d4a40385b0861
Marcin Czenko <marcin@everydayproductive.com>
2020-03-17 23:41:37 +0100

    idservice: updates node-ipc demo to demonstrate how to communicate with more than one server using one client instance

--- workspaces/idservice/demo-node-ipc/node-ipc-client.js
+++ workspaces/idservice/demo-node-ipc/node-ipc-client.js
@@ -1,37 +1,76 @@
 import ipc from 'node-ipc'
+import commander from 'commander'
 
-ipc.config.appspace = 'crocs.'
-ipc.config.id = 'authentication-service'
-ipc.config.retry = 1000
+const program = new commander.Command()
 
-ipc.connectTo(
-  ipc.config.id,
-  () => {
-    ipc.of[ipc.config.id].on(
-      'connect',
-      () => {
-        ipc.log(`## connected to ${ipc.config.appspace}${ipc.config.id} ##`)
-        ipc.of[ipc.config.id].emit(
-          'message',
-          {
-            from: 'idservice',
-            message: 'hello'
-          }
-        )
-      }
-    )
-    ipc.of[ipc.config.id].on(
-      'disconnect',
-      () => {
-        ipc.log(`disconnected from ${ipc.config.appspace}${ipc.config.id}`)
-      }
-    )
-    ipc.of[ipc.config.id].on(
-      'message',
-      (data) => {
-        ipc.log(`got a message from ${ipc.config.appspace}${ipc.config.id}: `, data)
-      }
-    )
-    console.log(ipc.of[ipc.config.id].destroy)
+const startClientIPC = ({ serviceNamespace, serviceName }) => {
+  ipc.config.retry = 1000
+
+  const socketName = `${serviceNamespace}.${serviceName}`
+
+  ipc.connectTo(
+    socketName,
+    `${ipc.config.socketRoot}${serviceNamespace}.${serviceName}`,
+    () => {
+      ipc.of[socketName].on(
+        'connect',
+        () => {
+          ipc.log(`## connected to ${serviceNamespace}.${serviceName} ##`)
+          ipc.of[socketName].emit(
+            'message',
+            {
+              id: 'idservice',
+              message: `hello [${serviceNamespace}.${serviceName}]`
+            }
+          )
+        }
+      )
+      ipc.of[socketName].on(
+        'disconnect',
+        () => {
+          ipc.log(`disconnected from ${serviceNamespace}.${serviceName}`)
+        }
+      )
+      ipc.of[socketName].on(
+        'message',
+        (data) => {
+          ipc.log(`got a message from ${serviceNamespace}.${serviceName}: `, data)
+        }
+      )
+      console.log(ipc.of[socketName].destroy)
+    }
+  )
+}
+
+const connectTo = (endPoint) => {
+  const [serviceNamespace, serviceName] = endPoint.split('.')
+  if (serviceName === undefined || serviceName.length === 0) {
+    console.log('missing service name: the path should be in the format: service-namespace.service-name')
+    return false
   }
-)
+  console.log(`Using socket path: ${ipc.config.socketRoot}${endPoint}`)
+  startClientIPC({ serviceNamespace, serviceName })
+}
+
+const run = async cmdObj => {
+  const { endPoint1, endPoint2 } = cmdObj
+  ipc.log('============ Service 1 ================')
+  connectTo(endPoint1)
+
+  if (endPoint2) {
+    ipc.log('============ Service 2 ================')
+    connectTo(endPoint2)
+  }
+}
+
+async function main () {
+  program
+    .version('0.1.0')
+    .requiredOption('-1, --end-point-1 <path>', 'point separated service path for service-1: service-name-space.service-name, eg. crocs.authentication-service')
+    .option('-2, --end-point-2 <path>', '[optional] point separated service path for service-2: service-name-space.service-name, eg. crocs.name-service')
+    .action(run)
+
+  await program.parse(process.argv)
+}
+
+main()
