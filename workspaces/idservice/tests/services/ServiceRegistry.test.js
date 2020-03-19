cc22e19579853ead64e6f6bb9aa6d0c180f3590c
Marcin Czenko <marcin@everydayproductive.com>
2020-03-19 01:49:11 +0100

    idservice: adds ServiceRegistry

---
+++ workspaces/idservice/tests/services/ServiceRegistry.test.js
@@ -0,0 +1,180 @@
+import { ServiceRegistry } from '../../src/services/ServiceRegistry'
+
+import fs from 'fs-extra'
+import path from 'path'
+
+describe('ServiceRegistry', () => {
+  const servicePath = 'service-namespace.servicename'
+  const anotherServicePath = 'another-service-namespace.servicename'
+
+  const serializerFileDir = path.resolve(process.cwd(), '.fixtures', 'idservice')
+  const serializerFilePath = path.resolve(serializerFileDir, 'Service.json')
+
+  let serviceRegistry
+
+  const prepareFixtureFile = () => {
+    fs.ensureDirSync(serializerFileDir)
+    fs.removeSync(serializerFilePath)
+  }
+
+  beforeEach(() => {
+    prepareFixtureFile()
+    serviceRegistry = new ServiceRegistry({
+      serializerFilePath
+    })
+  })
+
+  afterEach(() => {
+    fs.removeSync(serializerFilePath)
+  })
+
+  it('does not have any services registred when created for the first time', () => {
+    expect(serviceRegistry.services).toEqual([])
+  })
+
+  it('allows registering a new service endpoit', () => {
+    serviceRegistry.register(servicePath)
+
+    expect(serviceRegistry.services).toEqual([servicePath])
+  })
+
+  it('maintains the order of registered services in the returned array', () => {
+    serviceRegistry.register(servicePath)
+    serviceRegistry.register(anotherServicePath)
+
+    expect(serviceRegistry.services).toEqual([servicePath, anotherServicePath])
+  })
+
+  describe('checking service path format', () => {
+    const pathExists = Symbol('path exists')
+    const noPath = Symbol('no path')
+    const emptyPath = Symbol('empty path')
+    const noServiceName = Symbol('no service name')
+    const noServiceNamespace = Symbol('no service namespace')
+    const tooManyPathComponents = Symbol('too many path components')
+
+    const errors = {
+      [pathExists]: 'Service with given path already exists!',
+      [noPath]: 'Attemting to register a service without providing path!',
+      [emptyPath]: 'Attemting to register a service with empty path!',
+      [noServiceName]: 'Missing service name in path!',
+      [noServiceNamespace]: 'Missing service namespace in path!',
+      [tooManyPathComponents]: 'Too many service path components!'
+    }
+
+    it('throws when trying to register existing path', () => {
+      const error = new Error(errors[pathExists])
+
+      serviceRegistry.register(servicePath)
+
+      expect(() => serviceRegistry.register(servicePath)).toThrow(error)
+    })
+
+    it('throws when path is not provided', () => {
+      const error = new Error(errors[noPath])
+
+      expect(() => serviceRegistry.register()).toThrow(error)
+    })
+
+    it('throws when path is provided but empty', () => {
+      const error = new Error(errors[noPath])
+
+      expect(() => serviceRegistry.register('')).toThrow(error)
+    })
+
+    it('throws when path does not include service name', () => {
+      const error = new Error(errors[noServiceName])
+
+      const serviceRegistry = new ServiceRegistry()
+
+      expect(() => serviceRegistry.register('serviceNamespace')).toThrow(error)
+      expect(() => serviceRegistry.register('serviceNamespace.')).toThrow(error)
+    })
+
+    it('throws when path does not include service namespace', () => {
+      const error = new Error(errors[noServiceNamespace])
+
+      expect(() => serviceRegistry.register('.serviceName')).toThrow(error)
+    })
+
+    it('throws when path includes too many path components', () => {
+      const error = new Error(errors[tooManyPathComponents])
+
+      expect(() => serviceRegistry.register('a.b.c.d')).toThrow(error)
+    })
+  })
+
+  describe('preserving state', () => {
+    const readSerializedState = () => {
+      try {
+        return JSON.parse(fs.readFileSync(serializerFilePath))
+      } catch {
+        return []
+      }
+    }
+
+    beforeEach(() => {
+      console.error = jest.fn()
+    })
+
+    afterEach(() => {
+      console.error.mockRestore()
+    })
+
+    it('writes registered services to a file when adding a new service', () => {
+      serviceRegistry.register(servicePath)
+
+      expect(readSerializedState()).toEqual(serviceRegistry.services)
+    })
+
+    it('writes more registered services to a file when adding a new service', () => {
+      serviceRegistry.register(servicePath)
+      serviceRegistry.register(anotherServicePath)
+
+      expect(readSerializedState()).toEqual(serviceRegistry.services)
+    })
+
+    it('restores previously registred services from a file', () => {
+      const serializedState = [
+        servicePath,
+        anotherServicePath
+      ]
+      fs.writeFileSync(serializerFilePath, JSON.stringify(serializedState))
+
+      serviceRegistry = new ServiceRegistry({
+        serializerFilePath
+      })
+
+      expect(serviceRegistry.services).toEqual(serializedState)
+    })
+
+    it('restores registred services to an empty list if file does not exist', () => {
+      serviceRegistry = new ServiceRegistry({
+        serializerFilePath
+      })
+
+      expect(serviceRegistry.services).toEqual([])
+    })
+
+    it('restores registred services to an empty list if file exists but is empty', () => {
+      fs.ensureFileSync(serializerFilePath)
+
+      serviceRegistry = new ServiceRegistry({
+        serializerFilePath
+      })
+
+      expect(serviceRegistry.services).toEqual([])
+    })
+
+    it('restores registred services to an empty list if file exists and is empty list', () => {
+      const serializedState = []
+      fs.writeFileSync(serializerFilePath, JSON.stringify(serializedState))
+
+      serviceRegistry = new ServiceRegistry({
+        serializerFilePath
+      })
+
+      expect(serviceRegistry.services).toEqual([])
+    })
+  })
+})
