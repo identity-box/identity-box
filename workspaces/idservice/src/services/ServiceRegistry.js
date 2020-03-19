ea2fbb5ec46257c10536c4acf8e8c60f6f805f0f
Marcin Czenko <marcin@everydayproductive.com>
2020-03-19 01:49:11 +0100

    idservice: adds ServiceRegistry

---
+++ workspaces/idservice/src/services/ServiceRegistry.js
@@ -0,0 +1,44 @@
+import path from 'path'
+import { StateSerializer } from '@identity-box/utils'
+
+class ServiceRegistry {
+  services = []
+  stateSerializer
+
+  constructor ({ serializerFilePath } = {}) {
+    const filePath = serializerFilePath || path.resolve(process.cwd(), 'Services.json')
+    this.stateSerializer = new StateSerializer(filePath)
+    this.services = this.stateSerializer.read() || []
+  }
+
+  checkPath = path => {
+    if (!path) {
+      throw new Error('Attemting to register a service without providing path!')
+    }
+
+    const [serviceNamespace, serviceName, ...rest] = path.split('.')
+
+    if (rest.length > 0) {
+      throw new Error('Too many service path components!')
+    }
+
+    if (serviceNamespace === undefined || serviceNamespace.length === 0) {
+      throw new Error('Missing service namespace in path!')
+    }
+
+    if (serviceName === undefined || serviceName.length === 0) {
+      throw new Error('Missing service name in path!')
+    }
+  }
+
+  register = servicePath => {
+    this.checkPath(servicePath)
+    if (this.services.includes(servicePath)) {
+      throw new Error('Service with given path already exists!')
+    }
+    this.services.push(servicePath)
+    this.stateSerializer.write(this.services)
+  }
+}
+
+export { ServiceRegistry }
