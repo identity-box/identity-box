afbc3a85ff391297a86eb5a6da484da3bf5e8534
Marcin Czenko <marcin@everydayproductive.com>
2020-06-04 00:02:14 +0200

    box-office: adds esm comment to index.js

--- workspaces/box-office/index.js
+++ workspaces/box-office/index.js
@@ -1,6 +1,11 @@
 #!/bin/sh 
 ":" //# http://sambal.org/?p=1014 ; exec /usr/bin/env node -r esm "$0" "$@"
 
+/**
+ * This is a convenience file that allows you to run the service in esm mode - so
+ * without babel transpilation in between
+ */
+
 import { main } from './source/main'
 
 main()
