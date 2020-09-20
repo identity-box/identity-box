26af0b4d1430e0ae880300b21c865c8dddafd4c2
Marcin Czenko <marcin@everydayproductive.com>
2020-09-20 20:55:11 +0200

    Adds extra check that the tunnel is established between sockets with the same tunnel id

--- workspaces/rendezvous/source/services/Tunnel.js
+++ workspaces/rendezvous/source/services/Tunnel.js
@@ -13,28 +13,45 @@ class Tunnel {
     this.start()
   }
 
-  onConnection = socket => {
-    console.log(`connection from socket with id: ${socket.id}`)
+  verifyTunnel = socket => {
     if (!this.socketSender) {
       console.log('SENDER')
       this.socketSender = socket
     } else if (!this.socketReceiver) {
-      console.log('RECEIVER')
-      this.socketReceiver = socket
+      if (this.tunnelIdFromSocket(this.socketSender) === this.tunnelIdFromSocket(socket)) {
+        console.log('RECEIVER')
+        this.socketReceiver = socket
+      } else {
+        throw new Error('The sockets at both ends of the tunnel are not having the same tunnel id!')
+      }
     } else {
-      console.log('too many connection attemts to the tunnel')
+      throw new Error('The sockets at both ends of the tunnel are not having the same tunnel id!')
+    }
+  }
+
+  onConnection = socket => {
+    console.log(`connection from socket with id: ${socket.id}`)
+    try {
+      this.verifyTunnel(socket)
+      socket.on('message', msg => {
+        this.onMessage(socket, msg)
+      })
+      socket.on('end', () => {
+        this.endTunnel(socket)
+      })
+      socket.on('disconnect', reason => {
+        console.log('Session disconnected:', socket.id, reason)
+      })
+    } catch (e) {
+      console.log(e.msg)
       this.endTunnel()
     }
+  }
 
-    socket.on('message', msg => {
-      this.onMessage(socket, msg)
-    })
-    socket.on('end', () => {
-      this.endTunnel(socket)
-    })
-    socket.on('disconnect', reason => {
-      console.log('Session disconnected:', socket.id, reason)
-    })
+  tunnelIdFromSocket = socket => {
+    // socket id has the form of /mnyiWEL4AbBJWUkpnIfc-k4hgHFOdnz8JHyjNDOTpG4#HxDrrxGFfCSa8YgHAAAB
+    // we first split on "#" and then ignore leading slash
+    return socket.id.split('#')[0].slice(1)
   }
 
   onMessage = async (socket, msg) => {
