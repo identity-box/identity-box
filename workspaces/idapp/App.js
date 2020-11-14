71441e3e9a05baa3d9a380e8ed36c87f8a5fc371
Marcin Czenko <marcin@everydayproductive.com>
2020-11-14 18:15:39 +0100

    Adds <<Calling getNode()...>> warning in Expo to <<ignoreLogs>>

--- workspaces/idapp/App.js
+++ workspaces/idapp/App.js
@@ -5,7 +5,8 @@ import { LogBox } from 'react-native'
 import { Buffer } from 'buffer/'
 
 LogBox.ignoreLogs([
-  'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?'
+  'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?',
+  'Calling `getNode()` on the ref of an Animated component is no longer necessary. You can now directly use the ref instead. This method will be removed in a future release.'
 ])
 
 window.Buffer = Buffer
