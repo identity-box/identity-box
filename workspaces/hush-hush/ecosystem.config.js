f1cd115868c880839a15206faf7d6b78e0c1a29d
Marcin Czenko <marcin@everydayproductive.com>
2022-07-30 15:13:18 +0200

    Reverst to the last stable version of the idapp

---
+++ workspaces/hush-hush/ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'hush-hush',
+    script: './node_modules/.bin/next',
+    args: 'start --port 3300',
+    instances: 1,
+    autorestart: false,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }]
+}
