0387d021e9a2e471a690a87a69e403f0c16d995e
Marcin Czenko <marcin@everydayproductive.com>
2022-07-31 13:58:23 +0200

    updates ignored warnings in App.js

--- workspaces/idapp/App.js
+++ workspaces/idapp/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Main } from 'src/views/main'
 import { LogBox } from 'react-native'
-import { Buffer } from 'buffer/'
+import { Buffer } from 'buffer'
 
 LogBox.ignoreLogs([
   'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?',
-  'Calling `getNode()` on the ref of an Animated component is no longer necessary. You can now directly use the ref instead. This method will be removed in a future release.'
+  'Calling `getNode()` on the ref of an Animated component is no longer necessary. You can now directly use the ref instead. This method will be removed in a future release.',
+  'EventEmitter.removeListener(\'change\' ...): Method has been deprecated. Please instead use `remove()` on the subscription returned by `EventEmitter.addListener`.'
 ])
 
 window.Buffer = Buffer
