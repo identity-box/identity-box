e32c175b14dbe3e974e72a8e6acbf5fea325340e
Marcin Czenko <marcin@everydayproductive.com>
2022-08-14 18:31:55 +0200

    idapp: updates configuration of the app

---
+++ workspaces/idapp/app.config.js
@@ -0,0 +1,45 @@
+function getName (config) {
+  if (process.env.APP_VARIANT === 'development') {
+    return `${config.name} (Dev)`
+  } else if (process.env.APP_VARIANT === 'preview') {
+    return `${config.name} (Preview)`
+  }
+  return config.name
+}
+
+function getBundleIdentifier (config) {
+  const bundleIdentifier = config.ios.bundleIdentifier
+  if (process.env.APP_VARIANT === 'development') {
+    return `${bundleIdentifier}.dev`
+  } else if (process.env.APP_VARIANT === 'preview') {
+    return `${bundleIdentifier}.preview`
+  }
+  return bundleIdentifier
+}
+
+function getAndroidPackage (config) {
+  const androidPackage = config.android.package
+  if (process.env.APP_VARIANT === 'development') {
+    return `${androidPackage}.dev`
+  } else if (process.env.APP_VARIANT === 'preview') {
+    return `${androidPackage}.preview`
+  }
+  return androidPackage
+}
+
+export default ({ config }) => {
+  console.log('using app.config.js')
+  console.log('process.env.APP_VARIANT=', process.env.APP_VARIANT)
+  return {
+    ...config,
+    name: getName(config),
+    ios: {
+      ...config.ios,
+      bundleIdentifier: getBundleIdentifier(config)
+    },
+    android: {
+      ...config.android,
+      package: getAndroidPackage(config)
+    }
+  }
+}
