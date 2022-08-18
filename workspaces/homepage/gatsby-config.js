e65c56eda9c14b24847bd5d70100296a670955ce
Marcin Czenko <marcin@everydayproductive.com>
2022-08-18 07:24:27 +0200

    homepage: remove redundant deps, adds gatsby-plugin-provide-react

--- workspaces/homepage/gatsby-config.js
+++ workspaces/homepage/gatsby-config.js
@@ -4,9 +4,8 @@ module.exports = {
     editBaseUrl: 'https://github.com/identity-box/identity-box/blob/master'
   },
   plugins: [
+    'gatsby-plugin-provide-react',
     '@confluenza/gatsby-theme-confluenza',
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-catch-links',
     'gatsby-plugin-root-import'
   ]
 }
