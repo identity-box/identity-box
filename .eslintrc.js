f8e15a0b214dbf1a9a63df6d817d09939b5683c3
Marcin Czenko <marcin.czenko@pm.me>
2022-12-20 03:22:55 +0100

    updates remaining dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -42,6 +42,16 @@ module.exports = {
     'react/prop-types': 0,
     'jsx-quotes': ['error', 'prefer-single'],
     'jest/expect-expect': 0,
-    'jest/no-standalone-expect': 0
+    'jest/no-standalone-expect': 0,
+    // Safe to disable the following rules as TSC will throw, ESLint doesn't understand interfaces properly,
+    // https://github.com/eslint/typescript-eslint-parser/issues/437
+    // source: https://github.com/ts-engine/ts-engine/blob/master/packages/eslint-config/index.js#L31
+    'no-undef': 'off',
+    // "@typescript-eslint/no-unused-vars",
+    'no-unused-vars': 'off',
+    'import/named': 'off',
+    'import/no-unresolved': 'off',
+    // we only use it in app/electron.server.ts because ignoring file does not seem to work
+    '@typescript-eslint/ban-ts-comment': 'off'
   }
 }
--- workspaces/homepage/gatsby-config.js
+++ workspaces/homepage/gatsby-config.js
@@ -1,11 +1,8 @@
 module.exports = {
+  jsxRuntime: 'automatic',
   siteMetadata: {
     title: 'Identity Box',
     editBaseUrl: 'https://github.com/identity-box/identity-box/blob/master'
   },
-  plugins: [
-    'gatsby-plugin-provide-react',
-    '@confluenza/gatsby-theme-confluenza',
-    'gatsby-plugin-root-import'
-  ]
+  plugins: ['@confluenza/gatsby-theme-confluenza', 'gatsby-plugin-root-import']
 }
