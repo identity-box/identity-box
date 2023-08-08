a6fe92d6351a6f6722909485d1964618d7ecbf31
Marcin Czenko <marcin.czenko@pm.me>
2023-08-08 15:15:43 +0200

    removes unused <main> router group

--- workspaces/idapp/app/(main)/index.jsx
+++
@@ -1,5 +0,0 @@
-import { Text } from 'react-native'
-
-export default function Index() {
-  return <Text>Index Screen</Text>
-}
--- workspaces/idapp/app/(main)/two.jsx
+++
@@ -1,5 +0,0 @@
-import { Text } from 'react-native'
-
-export default function TabOneScreen() {
-  return <Text>Tab Two Screen</Text>
-}
